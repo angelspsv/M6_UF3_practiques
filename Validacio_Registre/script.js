f2dbdb3d7e7e82da229e3f9231d716e9219b5923
angel_ivanov <angelspsv@gmail.com>
2024-02-27 00:00:31 +0100

    fets els camps del formulari

--- Validacio_Registre/script.js
+++ Validacio_Registre/script.js
@@ -3,6 +3,9 @@ let form = document.getElementById('registroForm');
 let nomInput = document.getElementById('nom');
 let emailInput = document.getElementById('email');
 let passwordInput = document.getElementById('password');
+let passwordInputConfirmacio = document.getElementById('password_confirm');
+let addressInput = document.getElementById('address');
+
 
 //funció per validar l'entrada de nom
 function validarNom(){
@@ -24,7 +27,8 @@ function validateEmail(email) {
 }
 
 function validarMail(){
-  let validacioMail = validateEmail(emailInput);
+  let emailValue = emailInput.value;
+  let validacioMail = validateEmail(emailValue);
   if (validacioMail){
     mailCorrecte(emailInput);
   } else {
@@ -44,8 +48,29 @@ function validaContrasenya(){
   }
 }
 
+//comparem ambdues contrasenyes
+function confirmPassword(){
+  let passwdEntrada = passwordInput.value.trim();
+  let passwdConfirmacio = passwordInputConfirmacio.value.trim();
+  if (passwdEntrada === passwdConfirmacio){
+    confPasswdCorrecte(passwordInputConfirmacio);
+  } else {
+    confPasswdError(passwordInputConfirmacio, 'Contrasenya de confirmació invalida');
+  }
+}
 
-    
+//funció per el camp d'adreça
+function validarAdresa(){
+  let adresaValue = addressInput.value.trim();
+  if (adresaValue == ''){
+    addressError(addressInput, 'La direcció no és correcta');
+  } else {
+    adresaCorrecte(addressInput);
+  }
+}
+
+
+//funcions per ser cridades si la validació és incorrecte
 function nomError(entrada, message) {
   entrada.nextElementSibling.innerText = message;
   entrada.classList.add('error');
@@ -61,6 +86,18 @@ function passwdError(entrada, message) {
   entrada.classList.add('error');
 }
 
+function confPasswdError(entrada, message) {
+  entrada.nextElementSibling.innerText = message;
+  entrada.classList.add('error');
+}
+
+function addressError(entrada, message) {
+  entrada.nextElementSibling.innerText = message;
+  entrada.classList.add('error');
+}
+
+
+//funcions per ser cridades si la validació és incorrecte
 function nomCorrecte(entrada) {
   entrada.nextElementSibling.innerText = '';
   entrada.classList.remove('error');
@@ -79,10 +116,26 @@ function passwdCorrecte(entrada) {
   entrada.classList.add('success');
 }
 
+function confPasswdCorrecte(entrada) {
+  entrada.nextElementSibling.innerText = '';
+  entrada.classList.remove('error');
+  entrada.classList.add('success');
+}
+
+function adresaCorrecte(entrada) {
+  entrada.nextElementSibling.innerText = '';
+  entrada.classList.remove('error');
+  entrada.classList.add('success');
+}
+
+
+
 nomInput.addEventListener('focusout', validarNom);
 emailInput.addEventListener('focusout', validarMail);
 passwordInput.addEventListener('focusout', validaContrasenya);
-    
+passwordInputConfirmacio.addEventListener('focusout', confirmPassword);
+addressInput.addEventListener('focusout', validarAdresa);
+
 
 form.addEventListener('submit', function(event) {
 // Evita l'enviament del formulari per defecte
@@ -91,10 +144,12 @@ form.addEventListener('submit', function(event) {
   validarNom();
   validarMail();
   validaContrasenya();
+  confirmPassword();
+  validarAdresa();
         
 
   // Si tot es valid s'envia
-  if (!nomInput.classList.contains('error') && !emailInput.classList.contains('error') && !passwordInput.classList.contains('error')) {
-    console.log('Formulario válido. Enviar datos al servidor...');
+  if (!nomInput.classList.contains('error') && !emailInput.classList.contains('error') && !passwordInput.classList.contains('error') && !passwordInputConfirmacio.classList.contains('error') && !addressInput.classList.contains('error')) {
+    console.log('Formulari vàlid. Enviar dades al servidor...');
   }
 });
